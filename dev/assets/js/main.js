f1e55be77dd7a12ba7a4f0a402ced7393d60c69f
Dedli <dedli888@gmail.com>
2018-02-18 21:12:11 +0200

    adaptive nav section

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,3 +1,14 @@
+// add active class to the current button (highlight it)
+
+var btns = document.getElementsByClassName("nav-item");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
 // owl carousel initialize
 
 $('.owl-carousel').owlCarousel({
