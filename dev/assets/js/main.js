57874606e8aff806e85ae8df5d3f722a8364033f
Dedli <dedli888@gmail.com>
2018-02-18 13:10:15 +0200

    footerslider section is done

---
+++ dev/assets/js/main.js
@@ -0,0 +1,37 @@
+// owl carousel initialize
+
+$('.owl-carousel').owlCarousel({
+    items: 4,
+    loop:true,
+    margin:30,
+    nav:true,
+    dots:false
+});
+
+// delete text in buttons in owl carousel
+
+$(".owl-prev").html("");
+$(".owl-next").html("");
+
+// change img in second slider
+
+(function () {
+    var item = document.querySelectorAll('.footerslider-item img');
+    for (var i = 0; i < item.length; i++) {
+        item[i].addEventListener("mouseover", setWhiteImg);
+
+        item[i].addEventListener("mouseout", setGrayImg);
+    }
+})();
+function setWhiteImg (){
+    var targetSrc = this.src;
+    var newSrc = targetSrc.slice(0, -4);
+    newSrc += "w.png";
+    this.src = newSrc;
+}
+function setGrayImg (){
+    var targetSrc = this.src;
+    var newSrc = targetSrc.slice(0, -5);
+    newSrc += ".png";
+    this.src = newSrc;
+}
