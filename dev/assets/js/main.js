eb1c8910ddf13f18aad9676c4074a81b016db1ad
Dedli <dedli888@gmail.com>
2018-02-19 11:29:27 +0200

    add active to advantages items

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,37 +1,54 @@
+// change bg of arrows in advantages section
+
+(function () {
+    var arrows = document.querySelectorAll(".arrow-box");
+    for (i = 0; i < arrows.length; i++) {
+        arrows[i].addEventListener("click", arrowsChangeBg);
+    }
+})();
+
+function arrowsChangeBg() {
+    var blueClass = document.getElementsByClassName("advantages-active-blue");
+    var blackClass = document.getElementsByClassName("advantages-active-black");
+    blueClass[0].className = blueClass[0].className.replace(" advantages-active-blue", "");
+    blackClass[0].className = blackClass[0].className.replace(" advantages-active-black", "");
+    this.querySelector(".arrow-box-black").className += " advantages-active-blue";
+    this.querySelector(".arrow-box-blue").className += " advantages-active-black";
+}
+
 // add active class to the current button (highlight it)
 
 var btns = document.getElementsByClassName("nav-item");
 for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("nav-active");
+        current[0].className = current[0].className.replace(" nav-active", "");
+        this.className += " nav-active";
     });
 }
 
 // owl carousel initialize
 
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    // margin:30,
-    nav:true,
-    dots:false,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:2,
-            center:true
+    loop: true,
+    nav: true,
+    dots: false,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 2,
+            center: true
         },
-        640:{
-            items:2,
-            center:true
+        640: {
+            items: 2,
+            center: true
         },
-        900:{
-            items:3,
-            center:true
+        900: {
+            items: 3,
+            center: true
         },
-        1170:{
-            items:4
+        1170: {
+            items: 4
         }
     }
 });
@@ -47,17 +64,18 @@ $(".owl-next").html("");
     var item = document.querySelectorAll('.footerslider-item img');
     for (var i = 0; i < item.length; i++) {
         item[i].addEventListener("mouseover", setWhiteImg);
-
         item[i].addEventListener("mouseout", setGrayImg);
     }
 })();
-function setWhiteImg (){
+
+function setWhiteImg() {
     var targetSrc = this.src;
     var newSrc = targetSrc.slice(0, -4);
     newSrc += "w.png";
     this.src = newSrc;
 }
-function setGrayImg (){
+
+function setGrayImg() {
     var targetSrc = this.src;
     var newSrc = targetSrc.slice(0, -5);
     newSrc += ".png";
